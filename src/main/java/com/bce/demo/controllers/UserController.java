c02482b76e9126fecda060057c5f3c12716d2dbb
Fabian <fabianjacho@gmail.com>
2025-07-08 17:52:30 -0500

    Creación de Api con SprinBoot, con busqueda por ID

--- src/main/java/com/bce/demo/controllers/UserController.java
+++ src/main/java/com/bce/demo/controllers/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,4 +35,9 @@ public class UserController {
     public User createUser(@RequestBody User user) {
         return userService.create(user);
     }   
+
+    @GetMapping("/{userId}")
+    public User getUserById(@PathVariable("userId") Integer userId) {
+        return userService.getUserById(userId);
+    }
 }
